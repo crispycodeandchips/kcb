80b4afbeb803d09d0ab2eafcdd2cba540bcda333
--unset <--unset>
2021-11-28 18:48:25 +0900

    cors 설정 추가

--- src/main/java/com/crispycode/kcb/config/WebConfig.java
+++ src/main/java/com/crispycode/kcb/config/WebConfig.java
@@ -5,13 +5,18 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class SecurityConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
     /**
      *
      * ※크로스 도메인 이슈: 브라우저에서 다른 도메인으로 URL 요청을 하는 경우 나타나는 보안문제
      */
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowCredentials(true).allowedOrigins("http://localhost:3000");
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST")
+                .maxAge(3000);
+
     }
 }
