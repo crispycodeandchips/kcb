7dbfbaf1a4f1f23896275f3afd28f9b2c7185736
Lydia Yoo <bb9e.bb8.c3po.r2d2@gmail.com>
2021-12-04 18:35:59 +0900

    Swagger 설정 추가

---
+++ src/main/java/com/crispycode/kcb/config/SwaggerConfig.java
@@ -0,0 +1,58 @@
+package com.crispycode.kcb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author MW
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .consumes(getConsumeContentTypes())
+                .produces(getProduceContentTypes())
+                .apiInfo(getApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.crispycode.kcb.controller"))
+                .paths(PathSelectors.ant("/**"))
+                .build();
+    }
+
+    private Set<String> getConsumeContentTypes() {
+        Set<String> consumes = new HashSet<>();
+        consumes.add("application/json;charset=UTF-8");
+        consumes.add("application/x-www-form-urlencoded");
+        return consumes;
+    }
+
+    private Set<String> getProduceContentTypes() {
+        Set<String> produces = new HashSet<>();
+        produces.add("application/json;charset=UTF-8");
+        return produces;
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("API")
+                .description("API")
+                .contact(new Contact("KCB Swagger", "", "bb9e.bb8.c3po.r2d2@gmail.com"))
+                .version("1.0")
+                .build();
+    }
+}
--- src/main/java/com/crispycode/kcb/controller/MainController.java
+++ src/main/java/com/crispycode/kcb/controller/MainController.java
@@ -6,6 +6,7 @@ import com.crispycode.kcb.model.BreweryEvent;
 import com.crispycode.kcb.model.Goods;
 import com.crispycode.kcb.service.BeerService;
 import com.crispycode.kcb.service.EventService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,7 @@ public class MainController {
     private final BeerService beerService;
     private final EventService eventService;
 
+    @ApiOperation(value = "메인 페이지의 신상 맥주 정보", notes = "출시일 기준 가장 최근 릴리즈 된 맥주 5개")
     @GetMapping(value = "/beers")
     public ResponseEntity<List<Beer>> getBeerList(){
 
